a570a2ea030047d8c1829cb6b4fbd1a8ec8851f5
DocDoWell <dandyowl84@gmail.com>
2024-06-28 13:53:53 +0100

    groupanagrams unit test updated

--- groupanagrams/GroupAnagrams_Test.java
+++ groupanagrams/GroupAnagrams_Test.java
@@ -2,13 +2,23 @@ package groupanagrams;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GroupAnagrams_Test {
     GroupAnagrams groupAnagrams = new GroupAnagrams();
 
     @Test
     public void test(){
-        assertEquals(3, groupAnagrams.groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}).size());
+        ArrayList<String> outputList1 = new ArrayList(Arrays.asList("eat", "tea", "ate"));
+        ArrayList<String> outputList2 = new ArrayList(Arrays.asList("tan", "nat"));
+        ArrayList<String> outputList3 = new ArrayList(Arrays.asList("bat"));
+        List<List<String>>  actualOutput = groupAnagrams.groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"});
+        assertEquals(3, actualOutput.size());
+        assertTrue(actualOutput.contains(outputList1) && actualOutput.contains(outputList2) && actualOutput.contains(outputList3));
     }
 }
